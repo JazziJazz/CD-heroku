f09f06ea0888bab5098626b06ee0e7bdbac803bd
Rodrigo Siliunas <64803354+JazziJazz@users.noreply.github.com>
2020-10-01 10:46:42 -0300

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,13 @@
 import os
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá mundo!"
+    data = [datetime().day, datetime().month, datetime().year] 
+    return 'Hoje é dia: ', data[0], '/', data[1], '/', data[2], '.'
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
