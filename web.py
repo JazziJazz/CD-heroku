f5b7838de6458a8ec6d295b1ece744bea0886422
Rodrigo Siliunas <64803354+JazziJazz@users.noreply.github.com>
2020-10-01 10:49:59 -0300

    Update web.py

--- web.py
+++ web.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     data = [datetime().day, datetime().month, datetime().year] 
-    return 'Hoje é dia: ', data[0], '/', data[1], '/', data[2], '.'
+    return f'Hoje é dia: {data[0]}/{data[1]}/{data[2]}. Bem-vindo, Rodrigo.'
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
